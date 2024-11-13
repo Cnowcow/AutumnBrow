f23084651ce62bc387a06031978b9de2fc15a2b3
nowcow <hhjnn92@icloud.com>
2024-11-13 18:31:03 +0900

    겹치는시간 예외처리ver.1 ok, 나머지 null값 처리 해보기, 시간 보여주는 방법 바꿔보기.

--- src/main/java/autumn/browmanagement/repository/ReservationRepository.java
+++ src/main/java/autumn/browmanagement/repository/ReservationRepository.java
@@ -1,19 +1,27 @@
 package autumn.browmanagement.repository;
 
-import autumn.browmanagement.Entity.Event;
-import autumn.browmanagement.Entity.Post;
+
 import autumn.browmanagement.Entity.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 
 @Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
 
-    List<Reservation> findByUserUserId(Long userId);
+    List<Reservation> findByUserUserIdOrderByReservationIdDesc(Long userId);
 
     List<Reservation> findAllByOrderByReservationIdDesc();
 
+
+    @Query("SELECT COUNT(r) > 0 FROM Reservation r " +
+            "WHERE r.reservationDate = :date " +
+            "AND ((r.reservationStartTime < :endTime AND r.reservationEndTime > :startTime))")
+    boolean existsOverlappingReservation(@Param("date") LocalDate date, @Param("startTime") LocalTime startTime, @Param("endTime") LocalTime endTime);
 
 }
